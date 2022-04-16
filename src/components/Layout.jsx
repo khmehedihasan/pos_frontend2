da375f6e45b45e0c6c40353d53e6671550be91fd
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-17 01:48:00 +0600

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 import Sidebar from './Sidebar'
 
 function Layout() {
+  const [bar, setBar] = useState('-ml-60');
+  const [mbar, setmBar] = useState(false);
   return (
-    <div className=' w-screen h-screen flex bg-slate-200 '>
-        <Sidebar />
-        <Nav />
+    <div className=' w-screen h-screen flex bg-slate-200 overflow-auto '>
+        <Sidebar bar={bar} mbar={mbar} />
+        <Nav setBar={setBar} bar={bar} setmBar={setmBar} mbar={mbar} />
     </div>
   )
 }
