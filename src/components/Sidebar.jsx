91e8a8fcd77c1b8f14f675f95ce08d033adfdcfa
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-17 15:15:02 +0600

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useRef, useState }from 'react'
+import React, { useState }from 'react'
 import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom';
-// import OutsideClick from '../hooks/OutsideClick';
 import SideBarToggler from './SideBarToggler';
 import MinSideBarToggler from './MinSideBarToggler';
 
 function Sidebar({bar, mbar}) {
 
     const [active, setActive] = useState(-1);
-    const [show, setShow] = useState('-ml-80');
-    const outClick = useRef(null);
-    // const outSideClick = OutsideClick(outClick);
-
-    // useEffect(()=>{
-    //     if(outSideClick){
-    //         setShow('-ml-80')
-    //     }
-    // },[outSideClick]);
-
+    console.log('sidebar')
 
     const data = [
         {
@@ -31,7 +21,7 @@ function Sidebar({bar, mbar}) {
         },
         {
             title:'Contacts',
-            icon: <i className="fa fas fa-address-book"></i>,
+            icon: <i className="fa fas fa-address-book  pr-2"></i>,
             links:[
                 {name:'Suppliers',link:'/'},
                 {name:'Customers',link:'/'},
@@ -85,7 +75,7 @@ function Sidebar({bar, mbar}) {
         },
         {
             title:'Stock Transfers',
-            icon: <i className="fa fas fa-truck "></i>,
+            icon: <i className="fa fas fa-truck  pr-2"></i>,
             links:[
                 {name:'List Stock Transfers',link:'/'},
                 {name:'Add Stock Transfer',link:'/'}
@@ -93,7 +83,7 @@ function Sidebar({bar, mbar}) {
         },
         {
             title:'Stock Adjustment',
-            icon: <i className="fa fas fa-database"></i>,
+            icon: <i className="fa fas fa-database  pr-2"></i>,
             links:[
                 {name:'List Stock Adjustments',link:'/'},
                 {name:'Add Stock Adjustment',link:'/'}
@@ -110,7 +100,7 @@ function Sidebar({bar, mbar}) {
         },
         {
             title:'Reports',
-            icon: <i className="fa-solid fa-chart-line"></i>,
+            icon: <i className="fa-solid fa-chart-line  pr-2"></i>,
             links:[
                 {name:'Profit / Loss Report',link:'/'},
                 {name:'Product Purchase Report',link:'/'},
@@ -148,27 +138,27 @@ function Sidebar({bar, mbar}) {
         {
           (mbar === false)?
           <div className={`${bar} transition-all duration-700 md:ml-0 w-60 h-screen shrink-0 flex flex-col`}>
-            <Link to='/'><div className=' w-60 py-4 h-16 bg-blue-500 text-center text-2xl text-white overflow-hidden'>SKY MART</div></Link>
+            <Link to='/dashboard'><div className=' w-60 py-4 h-16 bg-blue-500 text-center text-2xl text-white overflow-hidden'>SKY MART</div></Link>
             <div className=' w-60 h-full bg-slate-50'>
-                <NavLink to='/dashboard'  onClick={()=> setShow('-ml-80')} className={({isActive})=>isActive? 'pt-4 no-underline hover:text-blue-300 block pl-6 text-blue-300 ':'pt-4 no-underline hover:text-blue-300 block pl-6 text-black '}  ><i className="fa-solid fa-chart-column pr-2"></i> Dashboard </NavLink>
+                <NavLink to='/dashboard' className={({isActive})=>isActive? 'pt-4 no-underline hover:text-blue-300 block pl-6 text-blue-300 ':'pt-4 no-underline hover:text-blue-300 block pl-6 text-black '}  ><i className="fa-solid fa-chart-column pr-2"></i> Dashboard </NavLink>
                 {
                     data.map((data,index)=>{
                         return(
-                            <SideBarToggler key={index} index={index} activeIndex={active} setShow={setShow} setActive={setActive} data={data} />
+                            <SideBarToggler key={index} index={index} activeIndex={active} setActive={setActive} data={data} />
                         )
                     })
                 }
             </div>
         </div>
           :
-          <div className=' w-14 transition-all duration-700 h-screen '>
+          <div className=' w-14 transition-all duration-700 h-screen flex flex-col '>
             <Link to='/'><div className=' w-full py-4 bg-blue-500 text-center text-2xl text-white'>SM</div></Link>
             <div className=' w-14 h-full bg-slate-50'>
-                <NavLink to='/dashboard'  onClick={()=> setShow('-ml-80')} className={({isActive})=>isActive? 'pt-4 no-underline hover:text-blue-300 block pl-2 text-blue-300 ':'pt-4 no-underline hover:text-blue-300 block pl-2 text-black '}  ><i className="fa-solid fa-chart-column pr-2"></i> </NavLink>
+                <NavLink to='/dashboard' className={({isActive})=>isActive? 'pt-4 no-underline hover:text-blue-300 block pl-4 text-blue-300 ':'pt-4 no-underline hover:text-blue-300 block pl-4 text-black '}  ><i className="fa-solid fa-chart-column pr-2"></i> </NavLink>
                 {
                     data.map((data,index)=>{
                         return(
-                            <MinSideBarToggler key={index} index={index} activeIndex={active} setShow={setShow} setActive={setActive} data={data} />
+                            <MinSideBarToggler key={index} index={index} activeIndex={active} setActive={setActive} data={data} />
                         )
                     })
                 }
