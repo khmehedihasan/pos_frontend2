86277f1a8266cad606a910ff7132b6e265dafde1
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-17 15:14:24 +0600

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom';
 import Nav from './Nav'
 import Sidebar from './Sidebar'
 
-function Layout() {
+function Layout({chindren}) {
   const [bar, setBar] = useState('-ml-60');
   const [mbar, setmBar] = useState(false);
+  console.log('layout')
   return (
-    <div className=' w-screen h-screen flex bg-slate-200 overflow-auto '>
+    <>
+      <div className=' w-screen h-screen flex bg-slate-200 overflow-hidden float-left'>
         <Sidebar bar={bar} mbar={mbar} />
-        <Nav setBar={setBar} bar={bar} setmBar={setmBar} mbar={mbar} />
-    </div>
+        <div className=' w-full h-full '>
+          <Nav setBar={setBar} bar={bar} setmBar={setmBar} mbar={mbar} />
+          <div className=' w-full h-screen overflow-auto '>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+
+    </>
+
   )
 }
 
