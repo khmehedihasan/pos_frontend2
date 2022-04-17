d583ba97e5680764c3722aed0230f7365b222f2d
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-17 15:14:13 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import '../src/assets/css/style.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigate to='/dashboard' />} />
+      <Route path='/' element={<Layout />} >
+        <Route path='dashboard' element={<Home />} />
+      </Route>
     </Routes>
   );
 }
