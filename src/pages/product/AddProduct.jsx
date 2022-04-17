ae8796626b455c48f1f78abed4f8ca7807ea6132
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-18 05:18:18 +0600

    added

---
+++ src/pages/product/AddProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddProduct() {
+    console.log('add product')
+  return (
+    <div className=''>
+        <div className=' w-screen bg-slate-600 h-96'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vel blanditiis asperiores veniam, voluptatem eligendi odio quo pariatur quisquam ad nulla? Mollitia esse quibusdam ipsa ducimus iure eaque nulla necessitatibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, magnam ab sapiente quod sit expedita facere in eveniet! Voluptatem, magnam. Cum nam quas ratione facere labore saepe nemo? Ullam, possimus?</div>
+    </div>
+  )
+}
+
+export default AddProduct
