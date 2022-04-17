56d9ba4c3bdd8659bc68a96bcd494e14b117abd8
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-18 05:16:05 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import '../src/assets/css/style.css';
+import './assets/css/custom.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
 import Layout from './components/Layout';
+import Home from './pages/Home';
+import AddProduct from './pages/product/AddProduct';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Route path='/' element={<Navigate to='/dashboard' />} />
       <Route path='/' element={<Layout />} >
         <Route path='dashboard' element={<Home />} />
+        <Route path='addProduct' element={<AddProduct />} />
       </Route>
     </Routes>
   );
