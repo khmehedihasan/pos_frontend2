20e1c5c1b740e597c4cda2b684decd22caa00ce3
khmehedihasan <mehedihasan47341@gmail.com>
2022-05-08 17:11:42 +0600

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -48,7 +48,7 @@ function Nav({bar, setBar, mbar, setmBar}) {
 
 
   return (
-    <nav className={`${mbar?'w-[calc(100%-56px)]':'md:w-3/4 lg:w-4/5 xl:w-10/12'} w-[calc(100%-56px)] transition-all duration-700  float-right h-screen-2 bg-blue-500 text-white flex items-center justify-between px-4 border-l border-b border-blue-400`}>
+    <nav className={`${mbar?'w-[calc(100%-56px)]':'md:w-3/4 lg:w-4/5 xl:w-10/12'} w-[calc(100%-56px)] transition-all duration-700  float-right h-screen-2 bg-dark-blue-2 text-white flex items-center justify-between px-4 border-l border-b border-blue-900`}>
       <div className=' w-full mr-2 md:mr-6 flex items-center justify-start md:justify-between'>
           {/* <div onClick={toggleBar} className=' md:hidden hover:bg-slate-700/25 hover:rounded-sm py-1 px-2 mr-3'><i className="fa-solid fa-bars"> b1</i></div> */}
           <div onClick={togglemBar} className=' hidden md:block hover:bg-slate-700/25 hover:rounded-sm py-1 px-2 mr-3'><i className="fa-solid fa-bars"></i></div>
@@ -66,11 +66,11 @@ function Nav({bar, setBar, mbar, setmBar}) {
             <div onMouseEnter={()=> setShow('block')} className=" w-14 h-14 bg-gray-500 rounded-full bg-[url('../images/user.jpg')] bg-center bg-cover bg-no-repeat"></div>
             <div className={ `${show} z-10 w-52 h-6  absolute top-20 -mt-1 right-1`}>            
             <div className=" absolute -top-5 w-full h-5"></div>
-                  <div className=" w-full h-24 p-4 bg-blue-500 before:w-4 before:h-4 before:absolute before:-top-2 before:right-5 before:bg-blue-500 before:rotate-45">
+                  <div className=" w-full h-24 p-4 bg-dark-blue-2 before:w-4 before:h-4 before:absolute before:-top-2 before:right-5 before:bg-dark-blue-2 before:rotate-45">
                       <h4 className="text-sm text-white">Admin</h4>
                       <h3 className=" text-md sm:text-lg text-white font-bold">MD. Mehedi Hasan</h3>
                   </div >
-                  <div className=" bg-slate-600 ">
+                  <div className=" bg-slate-400 ">
                       <NavLink  className=" w-full py-2 pl-3 cursor-pointer hover:bg-yellow-400 hover:text-white block" to="/" ><i className="fa-solid fa-user"></i> My Profile</NavLink>
                       <NavLink  className=" w-full py-2 pl-3 cursor-pointer hover:bg-blue-400 hover:text-white block" to="/" ><i className="fa-solid fa-gear"></i> Settings</NavLink>
                       <button onClick={logOut}  className=" w-full py-2 pl-3 hover:bg-red-600 hover:text-white text-left"><i className="fa-solid fa-right-from-bracket"></i> Log Out</button>
