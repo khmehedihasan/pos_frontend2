6ad17a86ce91ae7fd27a748e6cfff0cd126c5351
khmehedihasan <mehedihasan47341@gmail.com>
2022-05-09 19:51:17 +0600

    upadate

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import loading from '../assets/images/loading.gif'
+
+function Loader() {
+  return (
+    <img className=' w-20' src={loading} alt="" />
+  )
+}
+
+export default Loader;
